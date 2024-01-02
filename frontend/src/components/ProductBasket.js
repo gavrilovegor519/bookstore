0d4807d2c337c452284d846ce8e307f1366e7d8e
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-01-02 11:57:45 +0300

    Add "remove from basket" feature.

--- frontend/src/components/ProductBasket.js
+++ frontend/src/components/ProductBasket.js
@@ -3,24 +3,12 @@ import './ProductBasket.css';
 
 function ProductBasket({id, image, title, price, setBasket}) {
 
-  /*
-  function deleteInBasket(arr, id) {
-    const entryId = arr.findIndex((obj) => obj.id === id)
-
-    if (entryId > -1) {
-      arr.splice(entryId, 1)
-    }
-
-    return arr
-  }
-  */
-
   return (
     <div className="ProductBasket">
       <img src={image} alt='Изображение товара'/>
       <h1>{title}</h1>
       <p>{price} рублей</p>
-      <button className='del'>Удалить</button>
+      <button className='del' onClick={() => setBasket(current => current.filter(product => {return product.id !== id}))}>Удалить</button>
     </div>
   );
 }
