ca2c691f51a23adf56e18969b5cbb21367a8874a
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-01-08 22:42:38 +0300

    Show basket link only registered users

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,11 +3,21 @@ import './Header.css';
 import UserBox from './UserBox';
 
 function Header({ setPage, setModalBox, token, setToken }) {
+  function BasketLink() {
+    if (token !== null) {
+      return(
+        <>
+          <li onClick={() => setPage('Basket')}>Корзина</li>
+        </>
+      )
+    }
+  }
+
   return (
     <div className="Header">
       <ul>
         <li onClick={() => setPage('Main')}>Главная</li>
-        <li onClick={() => setPage('Basket')}>Корзина</li>
+        <BasketLink/>
       </ul>
       <UserBox setModalBox={setModalBox} token={token} setToken={setToken} setPage={setPage} />
     </div>
