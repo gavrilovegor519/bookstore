decbebf584d5f67dcc9d6c0f814c28fb4f0fc270
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-01-09 20:51:36 +0300

    Fix product QTY problem

--- frontend/src/views/Basket.js
+++ frontend/src/views/Basket.js
@@ -3,35 +3,25 @@ import './Basket.css';
 import ProductBasket from '../components/ProductBasket';
 
 function Basket({ basket, setBasket, basketPrice, setBasketPrice, basketQty, setBasketQty }) {
-  function BasketContent() {
+  function ShowOrderButton() {
     if (basketQty > 0) {
-      return(
-        <>
-          <h1>Корзина</h1>
-          <div className="BasketContent">
-            {basket.map((item) => <ProductBasket key={item.id} id={item.id}
-              image={item.image} title={item.title}
-              price={item.price} setBasket={setBasket}
-              setBasketPrice={setBasketPrice}
-              setBasketQty={setBasketQty} />)}
-          </div>
-          <p>Количество товаров: {basketQty}</p>
-          <p>Общая стоимость товаров: {basketPrice}</p>
-          <button className="order">Оформить заказ</button>
-        </>
-      )
-    } else {
-      return(
-        <>
-          <h1>Корзина пуста!</h1>
-        </>
-      )
+      return(<><button className="order">Оформить заказ</button></>)
     }
   }
 
   return (
     <div className="Basket">
-      <BasketContent/>
+      <h1>Корзина</h1>
+      <div className="BasketContent">
+        {basket.map((item) => <ProductBasket key={item.id} id={item.id}
+          image={item.image} title={item.title}
+          price={item.price} setBasket={setBasket}
+          setBasketPrice={setBasketPrice}
+          setBasketQty={setBasketQty} />)}
+      </div>
+      <p>Количество товаров: {basketQty}</p>
+      <p>Общая стоимость товаров: {basketPrice}</p>
+      <ShowOrderButton/>
     </div>
   );
 }
