409fba7504c677c6a0f84b64d3d67d6cbe35874d
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-01-09 19:44:47 +0300

    Hide basket while is empty.

--- frontend/src/views/Basket.js
+++ frontend/src/views/Basket.js
@@ -3,19 +3,35 @@ import './Basket.css';
 import ProductBasket from '../components/ProductBasket';
 
 function Basket({ basket, setBasket, basketPrice, setBasketPrice, basketQty, setBasketQty }) {
+  function BasketContent() {
+    if (basketQty > 0) {
+      return(
+        <>
+          <h1>Корзина</h1>
+          <div className="BasketContent">
+            {basket.map((item) => <ProductBasket key={item.id} id={item.id}
+              image={item.image} title={item.title}
+              price={item.price} setBasket={setBasket}
+              setBasketPrice={setBasketPrice}
+              setBasketQty={setBasketQty} />)}
+          </div>
+          <p>Количество товаров: {basketQty}</p>
+          <p>Общая стоимость товаров: {basketPrice}</p>
+          <button className="order">Оформить заказ</button>
+        </>
+      )
+    } else {
+      return(
+        <>
+          <h1>Корзина пуста!</h1>
+        </>
+      )
+    }
+  }
+
   return (
     <div className="Basket">
-      <h1>Корзина</h1>
-      <div className="BasketContent">
-        {basket.map((item) => <ProductBasket key={item.id} id={item.id}
-          image={item.image} title={item.title}
-          price={item.price} setBasket={setBasket}
-          setBasketPrice={setBasketPrice}
-          setBasketQty={setBasketQty} />)}
-      </div>
-      <p>Количество товаров: {basketQty}</p>
-      <p>Общая стоимость товаров: {basketPrice}</p>
-      <button className="order">Оформить заказ</button>
+      <BasketContent/>
     </div>
   );
 }
